53b0405e23d7254f6b8be94bc0323623d8eb6d8e
Luis Garcia Gisbert <luisgg@gmail.com>
2026-09-14 09:41:04 +0200

    Updated create-image scripts

--- clonezilla/create-image/xz-small-image-slow-as-hell.sh
+++ clonezilla/create-image/xz-small-image-slow-as-hell.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 partclone.ext4 -c -s /dev/sda2 |xz -9e -c > sda2-image.xz
+# 2 hrs (VBox VM machine, same machine as stz) to compress 26.5 GB (16.6 GB in use) to 5.6 GB
+# restore time ???????
 #restore using xzcat sda2-image.xz | ...
--- clonezilla/create-image/zstd-recommended.sh
+++ clonezilla/create-image/zstd-recommended.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-partclone.ext4 -c -s /dev/sda2 |xz -19 -T0 > sda2-image.zst
+partclone.ext4 -c -s /dev/sda2 |zstd -19 -T0 > sda2-image.zst
+# 1:22:19 (VBox VM machine, same machine as xz) to compress 26.5 GB (16.6 GB in use) to 6.0 GB
 #restore using zstdcat sda2-image.zst | ...
